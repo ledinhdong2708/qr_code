395dc10b78c72e23396eb8ff2cdd51936eaf84f4
sangnguyen02 <ntsang101002@gmail.com>
2024-07-09 08:45:20 +0700

    add docentry to grr

--- lib/service/goodreturn_service.dart
+++ lib/service/goodreturn_service.dart
@@ -1,7 +1,10 @@
 // lib/fetch_data.dart
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:qr_code/constants/urlApi.dart';
+
+import '../component/dialog.dart';
 
 Future<Map<String, dynamic>?> fetchOprrData(String resultCode) async {
   final url = '$serverIp/api/v1/oprr/$resultCode';
@@ -43,3 +46,66 @@ Future<Map<String, dynamic>?> fetchPrr1Data(String resultCode) async {
     return null;
   }
 }
+
+
+Future<void> updateGrrDatabase(
+    String resultCode, String remake, BuildContext context) async {
+  final data = await fetchOprrData(resultCode);
+  if (data == null || data.isEmpty) {
+    print('No data to update');
+    return;
+  }
+
+  var updatedData = {
+    'docentry': data['data']['DocEntry'].toString(),
+    'docno': data['data']['DocNum'].toString(),
+    'postday': data['data']['DocDate'].toString(),
+    'vendorcode': data['data']['CardCode'],
+    'vendorname': data['data']['CardName'],
+    'remake': remake,
+  };
+
+  var url = Uri.parse('$serverIp/api/v1/grr');
+  var response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(updatedData),
+  );
+
+  if (response.statusCode == 200) {
+    print('Update successful');
+    print(updatedData);
+    CustomDialog.showDialog(context, 'Cập nhật thành công!', 'success');
+  } else {
+    print('Failed to update');
+    CustomDialog.showDialog(context, 'Cập nhật thất bại!', 'error');
+  }
+}
+
+Future<void> postData(Map<String, dynamic> data, BuildContext context) async {
+  const String url = '$serverIp/api/v1/grritems';
+
+  try {
+    var response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      print('Data successfully sent to server');
+      CustomDialog.showDialog(context, 'Cập nhật thành công!', 'success');
+    } else {
+      print('Failed to send data. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      CustomDialog.showDialog(context, 'Cập nhật thất bại!', 'error');
+    }
+  } catch (e) {
+    print('Error during POST request: $e');
+  }
+}
+
